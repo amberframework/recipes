084d49e77b4e393aad0ed86162865941594929d1
Faustino Aguilar <faustinoaq@gmail.com>
2018-04-30 11:43:30 -0500

    Reduce spec verbosity to no reach Travis log limit (10000)

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -2,18 +2,20 @@ require "spec"
 
 def build_amber
   unless File.exists?("./bin/amber")
-    system("shards build amber --production")
+    `shards install --production`
+    system("shards build amber")
   end
 end
 
 def generate_app(recipe)
-  system("./bin/amber new app -r `pwd`/#{recipe} --deps")
+  `./bin/amber new app -r $(pwd)/#{recipe} --deps"`
   system("cd app && ../bin/amber g scaffold Category title:string")
   system("cd app && ../bin/amber g scaffold Product title:string description:text category:reference")
   system("cd app && ../bin/amber g scaffold Comment body:text product:reference")
 end
 
 def build_app
+  `cd app && shards install --production`
   system("cd app && shards build app")
 end
 
