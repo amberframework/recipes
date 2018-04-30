b05473eeaa4ac5d8fbc3577542ef79f5f8076180
Faustino Aguilar <faustinoaq@gmail.com>
2018-04-30 11:27:48 -0500

    Add amber dependency and target

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,18 +1,16 @@
 require "spec"
 
 def build_amber
-  unless File.exists?("amber/bin/amber")
-    system("rm -rf amber")
-    system("git clone https://github.com/amberframework/amber.git")
-    system("cd amber; make")
+  unless File.exists?("./bin/amber")
+    system("shards build amber --production")
   end
 end
 
 def generate_app(recipe)
-  system("amber/bin/amber new app -r `pwd`/#{recipe} --deps")
-  system("cd app && ../amber/bin/amber g scaffold Category title:string")
-  system("cd app && ../amber/bin/amber g scaffold Product title:string description:text category:reference")
-  system("cd app && ../amber/bin/amber g scaffold Comment body:text product:reference")
+  system("./bin/amber new app -r `pwd`/#{recipe} --deps")
+  system("cd app && ../bin/amber g scaffold Category title:string")
+  system("cd app && ../bin/amber g scaffold Product title:string description:text category:reference")
+  system("cd app && ../bin/amber g scaffold Comment body:text product:reference")
 end
 
 def build_app
