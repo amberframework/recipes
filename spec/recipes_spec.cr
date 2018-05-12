e70f3ca81875e8b158aaab17a4955b4c76bab8c0
Faustino Aguilar <faustinoaq@users.noreply.github.com>
2018-05-12 18:51:21 -0500

    Uncomment crecto recipe spec

--- spec/recipes_spec.cr
+++ spec/recipes_spec.cr
@@ -19,14 +19,12 @@ describe "Compile available recipes" do
     cleanup
   end
 
-  # TODO: crecto scaffolding is broken
-  # the crecto builtin template is also broken
-  # it "compiles and tests basic/crecto recipe" do
-  #   generate_app recipe: "basic/crecto"
-  #   build_app.should be_true
-  # ensure
-  #   cleanup
-  # end
+  it "compiles and tests basic/crecto recipe" do
+    generate_app recipe: "basic/crecto"
+    build_app.should be_true
+  ensure
+    cleanup
+  end
 
   it "compiles and tests misc/modular recipe" do
     generate_app recipe: "misc/modular"
