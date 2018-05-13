20030b4ad3ab2cc1a5c319572b75596e49acdb1d
Faustino Aguilar <faustinoaq@gmail.com>
2018-05-12 21:26:00 -0500

    Fis deploy script

--- scripts/build_zips.sh
+++ scripts/build_zips.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
-# for any recipe that has been updated
-# rebuild the zip file in dist
+# This script is automatically triggered by Travis CI
+# for any recipe that has been updated on master.
 
 build_zip() {
 
-  echo build_zip $1, $2, $3
-  mkdir -p $2
-  dest=`pwd`/$2/$3
+  echo build_zip $1, $2
+  dest=`pwd`/$2
   (cd $1 && zip -r $dest *)
 }
 
 check_zip() {
-  dist=dist/`dirname $1`
-  zip=`basename $1`
-  dest=${dist}/${zip}
+  if [[ `dirname $1` == '.' ]]; then
+    zip=`basename $1`
+  else
+    zip=`dirname $1`_`basename $1`
+  fi
+
+  mkdir -p dist
+
+  dest=dist/${zip}
 
   #echo "check_zip" $1, $dist, $zip, $dest
   if [ ! -f ${dest}.zip ]; then
-    build_zip $1 $dist $zip $dest
+    build_zip $1 $dest
   else
     result=`find $1 -newer ${dest}.zip`
     #echo $result
     if [ ! -z "$result" ]; then
-      build_zip $1 $dist $zip $dest
+      build_zip $1 $dest
     fi
   fi
 }
@@ -31,6 +36,7 @@ check_zip() {
 find_recipes() {
   for f in `find . -path ./lib -prune -o -name 'app' -print`
   do
+    # echo `dirname $f | sed -e 's#./##'`
     check_zip `dirname $f | sed -e 's#./##'`
   done
 }
