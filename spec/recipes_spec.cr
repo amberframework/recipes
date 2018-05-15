f15618073e391fe48d570481f9b1b19bd1a28c98
Faustino Aguilar <faustinoaq@users.noreply.github.com>
2018-05-14 21:06:06 -0500

    Add automatic deploy (#14)

--- spec/recipes_spec.cr
+++ spec/recipes_spec.cr
@@ -1,39 +1,3 @@
 require "./spec_helper"
 
 build_amber
-
-describe "Compile available recipes" do
-  it "compiles and tests default recipe" do
-    generate_app recipe: "default"
-    build_app.should be_true
-    test_app.should be_true
-  ensure
-    cleanup
-  end
-
-  it "compiles and tests basic/granite recipe" do
-    generate_app recipe: "basic/granite"
-    build_app.should be_true
-    test_app.should be_true
-  ensure
-    cleanup
-  end
-
-  # TODO: crecto scaffolding is broken
-  # the crecto builtin template is also broken
-  # it "compiles and tests basic/crecto recipe" do
-  #   generate_app recipe: "basic/crecto"
-  #   build_app.should be_true
-  # ensure
-  #   cleanup
-  # end
-
-  it "compiles and tests misc/modular recipe" do
-    generate_app recipe: "misc/modular"
-    build_app.should be_true
-    test_app.should be_true
-  ensure
-    cleanup
-  end
-
-end
