11391b6a544b40e142a21aa504c0ab41e64c8c93
Faustino Aguilar <faustinoaq@gmail.com>
2018-05-17 03:13:19 -0500

    Fixes basic crecto recipe

--- spec/basic_crecto_spec.cr
+++ spec/basic_crecto_spec.cr
@@ -1,8 +1,6 @@
 require "./recipes_spec"
 
 describe "Basic Crecto" do
-  # TODO: crecto scaffolding is broken
-  # the crecto builtin template is also broken
   it "compiles and tests basic/crecto recipe" do
     generate_app recipe: "basic/crecto"
     build_app.should be_true
