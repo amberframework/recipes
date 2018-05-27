e1a7943667c0673234b95c8f6fcf0eeeb86b894a
Faustino Aguilar <faustinoaq@gmail.com>
2018-05-27 18:48:04 -0500

    Patch react recipe specs

--- spec/react_preact_redux_spec.cr
+++ spec/react_preact_redux_spec.cr
@@ -1,8 +1,52 @@
 require "./recipes_spec"
 
+require "file_utils" # HACK: patch_amber dependency
+
+# HACK: delete after merge https://github.com/amberframework/amber/pull/774
+def patch_amber
+  system("cd app && shards install")
+  FileUtils.rm_rf("app/lib/amber/src/amber/controller/helpers/responders.cr")
+end
+
 describe "React Preact-Redux" do
-  it "compiles and tests react/preact_redux recipe" do
-    generate_app recipe: "react/preact_redux"
+  it "compiles and tests react/preact_redux recipe with slang and granite" do
+    generate_app(recipe: "react/preact_redux").should be_true
+
+    patch_amber
+
+    build_app.should be_true
+    test_app.should be_true
+  ensure
+    cleanup
+  end
+
+  it "compiles and tests react/preact_redux recipe with ecr and granite" do
+    generate_app(recipe: "react/preact_redux", template: "ecr").should be_true
+
+    patch_amber
+
+    build_app.should be_true
+    test_app.should be_true
+  ensure
+    cleanup
+  end
+
+  it "compiles and tests react/preact_redux recipe with slang and crecto" do
+    generate_app(recipe: "react/preact_redux", template: "slang", model: "crecto").should be_true
+
+    patch_amber
+
+    build_app.should be_true
+    test_app.should be_true
+  ensure
+    cleanup
+  end
+
+  it "compiles and tests react/preact_redux recipe with ecr and crecto" do
+    generate_app(recipe: "react/preact_redux", template: "ecr", model: "crecto").should be_true
+
+    patch_amber
+
     build_app.should be_true
     test_app.should be_true
   ensure
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -2,21 +2,35 @@ require "spec"
 
 def build_amber
   unless File.exists?("./bin/amber")
-    `shards install --production`
+    system("shards install --production")
     system("shards build amber")
   end
 end
 
-def generate_app(recipe)
-  `./bin/amber new app -r $(pwd)/#{recipe} -d sqlite --deps`
+def generate_app(recipe, template = "slang", model = "granite")
+  system("./bin/amber new app -r $(pwd)/#{recipe} -d sqlite -t #{template} -m #{model}")
+  system("cd app && ../bin/amber g controller Information all")
+
+  # TODO: uncomment after https://github.com/amberframework/amber/pull/818 is merged
+  # system("cd app && ../bin/amber g model Data title:string body:text")
+
+  # FIXME: see https://github.com/amberframework/amber/pull/815
+  if model == "granite"
+    system("cd app && ../bin/amber g auth User")
+  else
+    system("cd app && ../bin/amber g model User email:string")
+  end
+
   system("cd app && ../bin/amber g scaffold Category title:string")
   system("cd app && ../bin/amber g scaffold Product title:string description:text category:reference")
   system("cd app && ../bin/amber g scaffold Comment body:text product:reference")
   system("cd app && ../bin/amber db migrate")
+
+  # TODO: see https://github.com/amberframework/recipes/pull/15
+  # system("cd app && crystal tool format src config spec --check")
 end
 
 def build_app
-  `cd app && shards install --production`
   system("cd app && shards build app")
 end
 
