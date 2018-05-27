7cc464df2b210bbf856a646940bbdcb3d0d5f428
Faustino Aguilar <faustinoaq@gmail.com>
2018-05-27 18:51:09 -0500

    Add HACKs as workaround for pending amber issues

--- react/preact_redux/app/config/initializers/responders.cr
+++ react/preact_redux/app/config/initializers/responders.cr
@@ -1,11 +1,27 @@
-# HACK: delete after https://github.com/amberframework/amber/pull/774 is merged
+# HACK: delete after merge https://github.com/amberframework/amber/pull/774
 module Amber::Controller::Helpers
   module Responders
     alias ProcType = Proc(String) | Proc(Int32)
 
     class Content
+      TYPE = {
+        html: "text/html",
+        json: "application/json",
+        txt:  "text/plain",
+        text: "text/plain",
+        xml:  "application/xml",
+      }
+
+      TYPE_EXT_REGEX         = /\.(#{TYPE.keys.join("|")})$/
+      ACCEPT_SEPARATOR_REGEX = /,|,\s/
+
+      @requested_responses : Array(String)
       @available_responses = Hash(String, String | ProcType).new
+      @type : String? = nil
       @body : String | Int32 | Nil = nil
+
+      def initialize(@requested_responses)
+      end
 
       # TODO: add JS type similar to rails.
       def html(html : String | ProcType)
@@ -25,8 +41,16 @@ module Amber::Controller::Helpers
         self
       end
 
+      def json(**args : Object)
+        json(args.to_h)
+      end
+
       def text(text : String | ProcType)
         @available_responses[TYPE[:text]] = text; self
+      end
+
+      def type
+        (@type ||= select_type).to_s
       end
 
       def body
@@ -39,6 +63,17 @@ module Amber::Controller::Helpers
           end
         end
       end
+
+      private def select_type
+        raise "You must define at least one response_type." if @available_responses.empty?
+        # NOTE: If only one response is requested or */* is present don't return anything else.
+        if @requested_responses.size != 1 || @requested_responses.includes?("*/*")
+          @requested_responses << @available_responses.keys.first
+        end
+        @requested_responses.find do |resp|
+          @available_responses.keys.includes?(resp)
+        end
+      end
     end
 
     def set_response(body, status_code = 200, content_type = Content::TYPE[:html])
@@ -49,6 +84,23 @@ module Amber::Controller::Helpers
       end
       context.response.content_type = content_type
       context.content = body
+    end
+
+    private def extension_request_type
+      path_ext = request.path.match(Content::TYPE_EXT_REGEX).try(&.[1])
+      return [Content::TYPE[path_ext]] if path_ext
+    end
+
+    private def accepts_request_type
+      accept = context.request.headers["Accept"]?
+      if accept && !accept.empty?
+        accepts = accept.split(";").first?.try(&.split(Content::ACCEPT_SEPARATOR_REGEX))
+        return accepts if !accepts.nil? && accepts.any?
+      end
+    end
+
+    private def requested_responses
+      extension_request_type || accepts_request_type || [] of String
     end
 
     protected def respond_with(status_code = 200, &block)
