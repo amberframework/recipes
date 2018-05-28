e01f2536537c0b401aab0f174890dae46f46a243
Faustino Aguilar <faustinoaq@gmail.com>
2018-05-28 18:58:00 -0500

    Remove HACKs

--- spec/react_preact_redux_spec.cr
+++ spec/react_preact_redux_spec.cr
@@ -1,19 +1,8 @@
 require "./recipes_spec"
 
-require "file_utils" # HACK: patch_amber dependency
-
-# HACK: delete after merge https://github.com/amberframework/amber/pull/774
-def patch_amber
-  system("cd app && shards install")
-  FileUtils.rm_rf("app/lib/amber/src/amber/controller/helpers/responders.cr")
-end
-
 describe "React Preact-Redux" do
   it "compiles and tests react/preact_redux recipe with slang and granite" do
     generate_app(recipe: "react/preact_redux").should be_true
-
-    patch_amber
-
     build_app.should be_true
     test_app.should be_true
   ensure
@@ -22,9 +11,6 @@ describe "React Preact-Redux" do
 
   it "compiles and tests react/preact_redux recipe with ecr and granite" do
     generate_app(recipe: "react/preact_redux", template: "ecr").should be_true
-
-    patch_amber
-
     build_app.should be_true
     test_app.should be_true
   ensure
@@ -33,9 +19,6 @@ describe "React Preact-Redux" do
 
   it "compiles and tests react/preact_redux recipe with slang and crecto" do
     generate_app(recipe: "react/preact_redux", template: "slang", model: "crecto").should be_true
-
-    patch_amber
-
     build_app.should be_true
     test_app.should be_true
   ensure
@@ -44,9 +27,6 @@ describe "React Preact-Redux" do
 
   it "compiles and tests react/preact_redux recipe with ecr and crecto" do
     generate_app(recipe: "react/preact_redux", template: "ecr", model: "crecto").should be_true
-
-    patch_amber
-
     build_app.should be_true
     test_app.should be_true
   ensure
