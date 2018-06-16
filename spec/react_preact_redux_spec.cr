8325f10c7292131f728c861079e94d2f3dce39c9
Faustino Aguilar <faustinoaq@gmail.com>
2018-06-16 18:41:24 -0500

    Uncomment react spec

--- spec/react_preact_redux_spec.cr
+++ spec/react_preact_redux_spec.cr
@@ -5,31 +5,31 @@ describe "React Preact-Redux" do
     generate_app(recipe: "react/preact_redux").should be_true
     build_app.should be_true
     test_app.should be_true
-  # ensure
-  #   cleanup
+  ensure
+    cleanup
   end
 
-  # it "compiles and tests react/preact_redux recipe with ecr and granite" do
-  #   generate_app(recipe: "react/preact_redux", template: "ecr").should be_true
-  #   build_app.should be_true
-  #   test_app.should be_true
-  # ensure
-  #   cleanup
-  # end
+  it "compiles and tests react/preact_redux recipe with ecr and granite" do
+    generate_app(recipe: "react/preact_redux", template: "ecr").should be_true
+    build_app.should be_true
+    test_app.should be_true
+  ensure
+    cleanup
+  end
 
-  # it "compiles and tests react/preact_redux recipe with slang and crecto" do
-  #   generate_app(recipe: "react/preact_redux", template: "slang", model: "crecto").should be_true
-  #   build_app.should be_true
-  #   test_app.should be_true
-  # ensure
-  #   cleanup
-  # end
+  it "compiles and tests react/preact_redux recipe with slang and crecto" do
+    generate_app(recipe: "react/preact_redux", template: "slang", model: "crecto").should be_true
+    build_app.should be_true
+    test_app.should be_true
+  ensure
+    cleanup
+  end
 
-  # it "compiles and tests react/preact_redux recipe with ecr and crecto" do
-  #   generate_app(recipe: "react/preact_redux", template: "ecr", model: "crecto").should be_true
-  #   build_app.should be_true
-  #   test_app.should be_true
-  # ensure
-  #   cleanup
-  # end
+  it "compiles and tests react/preact_redux recipe with ecr and crecto" do
+    generate_app(recipe: "react/preact_redux", template: "ecr", model: "crecto").should be_true
+    build_app.should be_true
+    test_app.should be_true
+  ensure
+    cleanup
+  end
 end
