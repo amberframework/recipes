44e1cb79f8cecc7c6faadf370aec8d069998ed69
Faustino Aguilar <faustinoaq@users.noreply.github.com>
2018-06-22 15:42:50 -0500

    Uncomment TODOs and HACKs (#23)

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -9,20 +9,9 @@ end
 
 def generate_app(recipe, template = "slang", model = "granite")
   system("./bin/amber new app -r $(pwd)/#{recipe} -d sqlite -t #{template} -m #{model}")
-
-  # TODO: uncomment after merge https://github.com/amberframework/amber/pull/823
-  # system("cd app && ../bin/amber g controller Information all")
-
-  # TODO: uncomment after merge https://github.com/amberframework/amber/pull/818
-  # system("cd app && ../bin/amber g model Data title:string body:text")
-
-  # HACK: fix after merge https://github.com/amberframework/amber/pull/824
-  if model == "granite"
-    system("cd app && ../bin/amber g auth User")
-  else
-    system("cd app && ../bin/amber g model User email:string")
-  end
-
+  system("cd app && ../bin/amber g controller Information all")
+  system("cd app && ../bin/amber g model Data title:string body:text")
+  system("cd app && ../bin/amber g auth User")
   system("cd app && ../bin/amber g scaffold Category title:string user_id")
   system("cd app && ../bin/amber g scaffold Product title:string description:text category:reference user_id")
   system("cd app && ../bin/amber g scaffold Comment body:text product:reference user_id")
